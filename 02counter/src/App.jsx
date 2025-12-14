d836440173604f309d8820f4b29fc053c6ab5164
PercyAKR <154340140+Percyakr2161@users.noreply.github.com>
2025-12-14 11:39:40 +0530

    Know About Hooks Deep Learning

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,15 +4,40 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [counter, setCounter]  = useState(15)
+
+  //let counter = 15
+
+  const addValue = () => {
+    //counter = counter + 1
+    setCounter(prevCounter => prevCounter + 1)
+    // setCounter(prevCounter => prevCounter + 1 )
+    // setCounter(prevCounter => prevCounter + 1)
+    // setCounter(prevCounter => prevCounter + 1)
+    
+  }
+
+  const removeValue = () => {
+    setCounter(counter - 1)
+  }
+  
   return (
     <>
-      <h1> RebotMachine With React </h1>
+      <h1>RobotMachine Using React </h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button
+      onClick={addValue}
+      >Add value {counter}</button> 
+      <br />
+      <button
+      onClick={removeValue}
+      >remove value {counter}</button>
+      <p>footer: {counter}</p>
     </>
   )
 }
 
 export default App
-
 

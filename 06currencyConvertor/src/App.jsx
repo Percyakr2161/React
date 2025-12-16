ebeff1fbcee6639b1cc8d77f59e2e9a4c8dcd48f
PercyAKR <154340140+Percyakr2161@users.noreply.github.com>
2025-12-16 13:32:01 +0530

    currency project Main File And Custom hooks

--- 06currencyConvertor/src/App.jsx
+++ 06currencyConvertor/src/App.jsx
@@ -1,16 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { InputBox } from "./components";
+import useCurrencyInfo from "./Hooks/useCurrencyInfo";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [amount, setAmount] = useState(1);
+  const [from, setFrom] = useState("USD");
+  const [to, setTo] = useState("INR");
+  const [convertedAmount, setConvertedAmount] = useState(0);
+
+  const currencyInfo = useCurrencyInfo(from);
+  const options = Object.keys(currencyInfo);
+
+  const swap = () => {
+    setFrom(to);
+    setTo(from);
+    setAmount(convertedAmount);
+    setConvertedAmount(amount);
+  };
+
+  const convert = () => {
+    if (!currencyInfo[to]) return;
+    setConvertedAmount(amount * currencyInfo[to]);
+  };
 
   return (
-    <>
-      <h1> Currancy App </h1>
-    </>
-  )
+    <div className="page">
+      <div className="converter-container">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            convert();
+          }}
+        >
+          <InputBox
+            label="From"
+            amount={amount}
+            currencyOptions={options}
+            onCurrencyChange={setFrom}
+            selectCurrency={from}
+            onAmountChange={setAmount}
+          />
+
+          <div className="swap-wrapper">
+            <button type="button" onClick={swap}>
+              swap
+            </button>
+          </div>
+
+          <InputBox
+            label="To"
+            amount={convertedAmount}
+            currencyOptions={options}
+            onCurrencyChange={setTo}
+            selectCurrency={to}
+            amountDisable
+          />
+
+          <button className="convert-btn">
+            Convert {from} → {to}
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
